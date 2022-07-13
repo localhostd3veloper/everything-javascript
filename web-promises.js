c30da299ae118e0b9ec53da0c8581080f5b26e4f
localhostd3veloper <gautamanand022@gmail.com>
2022-07-13 12:37:51 +0530

    JSON Placeholder Fetch

--- web-promises.js
+++ web-promises.js
@@ -46,6 +46,10 @@ const promise3 = new Promise((resolve, reject) => {
   setTimeout(resolve, 2000, "Bye");
 });
 
-Promise.all([promise1, promise2, promise3]).then((values) => {
+const promise4 = fetch("https://jsonplaceholder.typicode.com/posts").then(
+  (result) => result.json()
+);
+
+Promise.all([promise1, promise2, promise3, promise4]).then((values) => {
   console.log({ values });
 });
